37f07b3ae9d3b4358385f607b24fc0dadeee3650
lus21 <lus21484@gmail.com>
2020-01-30 01:38:41 +0400

    read transform and write stream

---
+++ streams/README.md
@@ -0,0 +1,6 @@
+Initialize project with npm,
+install moment.js,
+run project using the run script,
+create an app that will write to a stream every second the current time formatted using moment.js
+and write that stream to the file.
+Use Readable, Writable and Transform APIs exposed from stream module
---
+++ streams/index.js
@@ -0,0 +1,53 @@
+const moment = require("moment");
+const { Readable, Writable, Transform } = require("stream");
+const fs = require('fs');
+
+
+class MyReadable extends Readable {
+  constructor() {
+    super();
+  }
+
+  _read(size) {
+    setTimeout(() => {
+      this.push(new Date().toString());
+    },1000);
+  }
+}
+
+class MyWritable extends Writable {
+  constructor(filePath) {
+    super();
+
+    this.fd = fs.openSync(filePath, 'w', (err, file) => {
+      if (err) throw err;
+    });
+  }
+
+  _write(chunk, encoding, callback) {
+    fs.appendFile(this.fd, chunk, (err) => {
+      if (err) throw err;
+      console.log('Saved!');
+      callback();
+    });
+  }
+}
+
+class MyTransform extends Transform {
+  constructor() {
+    super();
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.push(moment(chunk.toString()).format());
+    callback();
+  }
+}
+
+const readStream = new MyReadable();
+const transformStream = new MyTransform();
+const writeStream = new MyWritable('./time.txt');
+
+readStream
+  .pipe(transformStream)
+  .pipe(writeStream);
